49cd0f2e02565802c5823f96796d669321c67b0f
Ritik Sharma <sharmaritik2002@gmail.com>
2024-12-05 23:39:20 +0530

    Change in error handler response object

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -9,14 +9,14 @@ const statusCodes = {
 const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || statusCodes.INTERNAL_SERVER_ERROR
     const env = process.env.NODE_ENV || `development`
-
+    
     if(env === 'production'){
         res.status(statusCode).json({
             message: err.message || 'Something went wrong. Please try again later.'
         })
     }else{
         res.status(statusCode).json({
-            error: err.message || 'Internal Server Error',
+            message: err.message || 'Internal Server Error',
             stack: err.stack
         })
     }
